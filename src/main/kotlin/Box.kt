b6d77c7bd9daa7f3b1e3c60a5682a87c77a1bc3c
kirito22037 <kirito22037@gmail.com>
2021-11-21 09:28:09 +0530

    basic class and object implementation

---
+++ src/main/kotlin/Box.kt
@@ -0,0 +1,12 @@
+class Box {
+    var length: Int = 10
+    var width: Int = 20
+    var height: Int = 5
+
+    fun fillContents() {
+        println("Box is filled")
+    }
+    fun open() {
+        println("Box Opened")
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("hello world")
+    // objects/instances of Box
+    val box1= Box()
+    val box2 = Box()
+
+    // Kotlin automatically create a default setter
+    box1.height = 7
+
+    // Kotlin automatically create a default getter to access the properties
+    println("Box 1 length : ${box1.length}")
+    println("Box 1 length : ${box1.width}")
+    println("Box 1 length : ${box1.height}")
+
+    box1.open()
+    box1.fillContents()
 }
