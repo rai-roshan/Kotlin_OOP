6d084fc910218e87330faa104f2c50be04962485
kirito22037 <kirito22037@gmail.com>
2021-11-21 11:20:30 +0530

    set method for volume property made private

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,16 +1,30 @@
+/*
+    there are 4 types
+    public: default, can be accessed everywhere
+    internal: available everywhere in the same module
+    private: available only inside the containing class
+    protected: same as private but available only inside subclasses or child classes
+ */
+
 class Box {
     var length: Int = 10
     var width: Int = 20
     var height: Int = 5
     var volume: Int = 0
         get() {
-            return length * width * height
+            return if( field == 0 )
+                length*width*height
+            else
+                field * 2
         }
-        set( value ) {
+        private set( value ) {
             field = value * 2
         }
 
-
+    // can't use setVolume as the function name
+    fun setVol(vol: Int ) {
+        volume = vol // this will call set method of volume property
+    }
     fun fillContents() {
         println("Box is filled")
     }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,8 +8,15 @@ fun main() {
 
     // Kotlin automatically create a default getter to access the properties
     println("Box 1 length : ${box1.length}")
-    println("Box 1 length : ${box1.width}")
-    println("Box 1 length : ${box1.height}")
+    println("Box 1 width : ${box1.width}")
+    println("Box 1 height : ${box1.height}")
+    println("Box 1 volume : ${box1.volume}")
+
+    /* this statement won't work since the getter method is set private
+        box1.volume = 10
+     */
+
+    box1.setVol(10)
     println("Box 1 volume : ${box1.volume}")
 
     box1.open()
