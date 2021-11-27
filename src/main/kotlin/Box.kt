5d99849fd4f1d5d6c382a4d48a8201d8d91f18b7
kirito22037 <kirito22037@gmail.com>
2021-11-27 10:21:49 +0530

    abstract class implementation

--- src/main/kotlin/Box.kt
+++ src/main/kotlin/Box.kt
@@ -1,6 +1,10 @@
-open class Box(val length: Int, val width: Int) {    // primary constructor with initialization of class property
-    open fun displayDimensions() {
-        println("length : ${this.length}")
-        println("width : ${this.width}")
+// abstract class means we can't create instances of it
+// it can and cannot have member implementation
+abstract class Box(val length: Int, val width: Int) {    // primary constructor with initialization of class property
+    // not implemented member , they are defined as abstract
+    abstract fun displayDimensions()
+    // implemented member
+    fun testClass() {
+        println("Box class is working")
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 fun main() {
     val box = ThreeDBox(10, 20, 30)
+    box.testClass()
     box.displayDimensions()
 }
